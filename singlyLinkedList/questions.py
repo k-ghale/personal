9a303c2f361c9d8d7090bfd1eb05d99caa3cb7d5
kabinghale <127985604+k-ghale@users.noreply.github.com>
2026-02-06 22:49:13 -0500

    Update questions.py

--- singlyLinkedList/questions.py
+++ singlyLinkedList/questions.py
@@ -18,18 +18,20 @@ class SinglyLinkedList:
             current = current.next
         
 
-
+# create a node
 n1 = Node(10)
 n2 = Node(10)
 n3 = Node(10)
 n4 = Node(10)
 
+#assigning next
 n1.next = n2
 n2.next = n3
 n3.next = n4
 
-
+#create a singly linked list
 l1 = SinglyLinkedList()
 l1.head = n1
 
+# traverse func
 l1.traverse()
