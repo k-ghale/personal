52bfe6fe1a2f09a49bea056f34891db951f30fc6
kabinghale <127985604+k-ghale@users.noreply.github.com>
2026-02-08 22:19:02 -0500

    Update App.jsx

--- guessthenumber/guess-the-number/src/App.jsx
+++ guessthenumber/guess-the-number/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Main App, Entry Point
 function App() {
 
   const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * 100) + 1 );
