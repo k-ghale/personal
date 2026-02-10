2d7211ea6085b45ed638c76c1a7fa0f2570247bc
k-ghale <kabinghale888@gmail.com>
2026-02-09 19:16:48 -0500

    recursion

---
+++ grokkingAlgorithm/chapter3/recursion.py
@@ -0,0 +1,32 @@
+
+#
+# def look_for_key(box):
+#     for item in box:
+#         if item.is_a_box():
+#             look_for_key(item)
+#         elif item.is_a_key():
+#             print(" key_found ");
+#
+
+# recursion function for a countdown
+def countdown(i):
+    if i == 0:
+        return
+
+    print(i)
+    return countdown(i-1)
+
+# func to calculate factorial
+    # 5! = 5 * 4 * 3 * 2 * 1
+def fact(x):
+    if x == 1:
+        return 1
+    return x * fact(x-1)
+
+
+# implementat
+
+num = 10
+
+countdown(num)
+print(fact(5))
---
+++ grokkingAlgorithm/chapter3/stack.py
@@ -0,0 +1,20 @@
+
+
+def greet(name):
+    print ("hello "+ name + " !")
+    greet2(name)
+    print ("getting ready to say goodbye!")
+    bye() 
+
+def greet2(name):
+    print ("how are you "+ name +" ?")
+
+def bye():
+    print ("ok bye")
+
+
+#implementaion
+name = "Kabin" 
+
+greet(name)
+
