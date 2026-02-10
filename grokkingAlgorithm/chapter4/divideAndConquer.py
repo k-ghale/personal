ec7a07998477655609b932bf9d29a2fddbd54be6
k-ghale <kabinghale888@gmail.com>
2026-02-10 15:32:13 -0500

    quicksort finished'

--- grokkingAlgorithm/chapter4/divideAndConquer.py
+++ grokkingAlgorithm/chapter4/divideAndConquer.py
@@ -1,3 +1,12 @@
 
+# recursive function to sum all
 
+arr = [1,2,3,4,5,6]
 
+def sum_recursion(arr, i=0):
+    if i == len(arr):
+        return 0
+    else:
+        return arr[i] + sum_recursion(arr[1:])
+
+print(sum_recursion(arr))
--- grokkingAlgorithm/chapter4/quicksort.py
+++ grokkingAlgorithm/chapter4/quicksort.py
@@ -1,9 +1,8 @@
 
-# to-do
+# swap in a different way
 
 a = 5
 b = 7
-
 
 a ^= b
 b ^= a
@@ -21,10 +20,44 @@ def sum(arr):
 
 # quick sort algorithm
 
+# short and simple one 
+def quickSort(arr):
+    if len(arr) < 2:
+        return arr
 
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:]  if i <= pivot]
+        
+        great = [i for i in arr[1:] if i > pivot]
+
+        return quickSort(less) + [pivot] + quickSort(great)
+
+# in depth
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+
+        lesser = []
+        greater = []
+
+        for i in arr[1:]:
+            if i <= pivot:
+                lesser.append(i)
+
+        for i in arr[1:]:
+            if i > pivot:
+                greater.append(i)
+        
+        return quicksort(lesser) + [pivot] + quicksort(greater)
 
 # implementation
 
 arr = [1,2,3,4,5]
+sort_this = [5,12,4,6,8,3]
 
 print(sum(arr))
+print(quickSort(sort_this))
+print(quicksort(sort_this))
