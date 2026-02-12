94e993a62d1b402061d6c2040c3592e5c2a413e7
kabinghale <127985604+k-ghale@users.noreply.github.com>
2026-02-11 23:36:19 -0500

    Update test.py

--- grokkingAlgorithm/chapter4/test.py
+++ grokkingAlgorithm/chapter4/test.py
@@ -3,3 +3,12 @@ arr = [12,2,3,4,5,5,5]
 
 for i in arr[0:]:
     print(i)
+
+# quicksort test
+
+def quicksort(arr):
+    pivot = arr[0]
+    lesser = [for i in arr[1:] if i < pivot]
+    greater = [for i in arr[1:] if i > pivot]
+    
+    return quicksort(lesser) + [pivot] + quicksort(greater)
