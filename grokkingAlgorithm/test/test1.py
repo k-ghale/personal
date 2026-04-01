7c498d390c34c4e4030f0cdff2845b74c7015b20
kabinghale <127985604+k-ghale@users.noreply.github.com>
2026-03-31 23:39:03 -0400

    Update test1.py

--- grokkingAlgorithm/test/test1.py
+++ grokkingAlgorithm/test/test1.py
@@ -1,7 +1,6 @@
 
 
 # binary search 
-
 def binarySearch(arr , guess):
     n = len(arr) - 1
     start = arr[0]
@@ -19,12 +18,11 @@ def binarySearch(arr , guess):
             end = mid - 1
 
 
-
+# implementation
 list = [1,2,3,4,5,6,7,8,9,10]
 print(binarySearch(list , 7))
 
 # selection sort
-
 def smallest(arr):
     smallest = arr[0]
     smallest_index = 0
@@ -41,19 +39,19 @@ def selectionSort(arr):
         new_arr.append(arr.pop(val))
     return new_arr
 
-
+#implementation
 arr = [5,6,4,56,3]
 print(smallest(list))
 print(selectionSort(arr))
 
 # recursion
-
 def countdown(i):
     if i == 0:
         return
     print(i)
     return countdown(i-1)
 
+# implementation
 countdown(10)
 
 def factorial(i):
@@ -61,7 +59,7 @@ def factorial(i):
         return i
     return i * factorial(i-1)
 
-
+# implementation
 print(factorial(5))
 
 
