fd27ad050b07c31c2d8a99f2b7b2c0a6b89afef4
kabinghale <kabinghale888@gmail.com>
2026-06-23 06:37:38 -0400

    created: a server

--- node/server.js
+++ node/server.js
@@ -3,7 +3,11 @@ import http from 'node:http'
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from the Server.")
+
+    const url = req.url
+    if (url == "/api" && req.method == 'GET'){
+        res.end("Hello from the Server.", 'utf8', () => console.log('response ended'))
+    }
 })
 
 server.listen(PORT, () => console.log("The server is listening on port:8000"))
