fb9cb9fcbd368141ecb52668b5c231475832bd6b
k-ghale <kabinghale888@gmail.com>
2026-07-08 03:54:22 -0400

    added:json folder

---
+++ json/server.js
@@ -0,0 +1,20 @@
+import http, { get } from 'node:http'
+import { getDataFromDB } from './database/db.js'
+
+const PORT = 8000
+
+ 
+const server = http.createServer((req, res) => {
+    // if (req.url === '/api' && req.method === 'GET'){
+    //     res.end('This is from the server')
+    // }
+    const destinations = await getDataFromDB()
+    if (req.url === '/api' && req.method === 'GET'){
+
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(destinations))
+    }
+}
+)
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
