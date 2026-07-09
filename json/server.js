32fdfb2a8172c9d5690ca2bc2a32830cda525f48
k-ghale <kabinghale888@gmail.com>
2026-07-09 04:33:13 -0400

    added: api

--- json/server.js
+++ json/server.js
@@ -1,5 +1,7 @@
 import http, { get } from 'node:http'
 import { getDataFromDB } from './database/db.js'
+import { error } from 'node:console'
+import { data } from './data.js'
 
 const PORT = 8000
 
@@ -14,6 +16,20 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode=200
         res.end(JSON.stringify(destinations))
+    }
+    else if(req.url.startsWith('/api/continent/') && req.method === 'GET'){
+        const continent = req.url.split('/').pop()
+        const filterData = destinations.filter((destinations) => {
+            return destinations.continent.toLowerCase() === continent.toLowerCase()
+        }) 
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode=200
+        res.send(JSON.stringify(filterData))
+    } 
+    else {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode=404
+        res.end(JSON.stringify({error:"not found", message: "The requested route does not exist."}))
     }
 }
 )
