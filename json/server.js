2606acc3a6272d41012622631a6ce1ea0e5cc388
k-ghale <kabinghale888@gmail.com>
2026-07-10 04:52:05 -0400

    update: abstraction

--- json/server.js
+++ json/server.js
@@ -3,30 +3,23 @@ import { getDataFromDB } from './database/db.js'
 import { error } from 'node:console'
 import { data } from './data.js'
 import { sendJSONresponse } from './util/sendJSONresponse.js'
+import { sendFilterData } from './util/sendFilterData.js'
 
 const PORT = 8000
 
  
 const server = http.createServer((req, res) => {
-    // if (req.url === '/api' && req.method === 'GET'){
-    //     res.end('This is from the server')
-    // }
     const destinations = await getDataFromDB()
     if (req.url === '/api' && req.method === 'GET'){
         sendJSONresponse(res, 200, destinations)
     }
     else if(req.url.startsWith('/api/continent/') && req.method === 'GET'){
-        const continent = req.url.split('/').pop()
-        const filterData = destinations.filter((destinations) => {
-            return destinations.continent.toLowerCase() === continent.toLowerCase()
-        })
+        const filteredData = sendFilterData(destinations, 'continent', continent) 
         sendJSONresponse(res, 200, filterData) 
     }
     else if(req.url.startsWith('/api/country') && req.method === 'GET'){
         const continent = req.url.split('/').pop()
-        const filterData = destinations.filter((destinations) => {
-            return destinations.continent.toLowerCase() === continent.toLowerCase()
-        })
+        const filteredData = sendFilterData(destinations, 'continent', continent) 
         sendJSONresponse(res, 200, filterData) 
     } 
     else {
---
+++ json/util/sendFilterData.js
@@ -0,0 +1,5 @@
+export const sendFilterData = (data, locationType, locationName) => {
+    return data.filter((destination) => {
+        return destination[locationType].toLowerCase() = locationName.toLowerCase()
+    }) 
+}
